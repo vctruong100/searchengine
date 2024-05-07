51dfcf491bb4e8ab5baa2ce85b5d8c88eaf5d66b
Jason Vu <jasondv2@uci.edu>
2024-05-06 17:59:49 -0700

    index/posting: Add Posting class

---
+++ index/posting.py
@@ -0,0 +1,16 @@
+# index/posting.py
+#
+# posting class
+
+class Posting:
+    def __init__(self, id, score, fields):
+        self.docid = id
+        self.score = score
+        self.fields = fields
+
+    def _encode(self):
+        pass
+
+    def _decode(self)
+        pass
+
