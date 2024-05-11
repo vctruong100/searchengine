931a15cfe882257214183eade22766937ad3c00b
VcTruong <vctruong100@gmail.com>
2024-05-10 17:17:11 -0700

    index/writer: remove whence in fh.seek()
    The whence argument is removed because it was causing a "ValueError:
    whence value 333245 unsupported" error.
    
    File handle opener in makeindex.py is changed to "w+b" from "r+b" to create file if file
    doesn't exist.

--- index/writer.py
+++ index/writer.py
@@ -80,7 +80,7 @@ def write_partial_index(index, docid, fh):
         ))
 
         # append
-        fh.seek(0, end_offset)
+        fh.seek(end_offset)
 
         # encode key value pairs
         # the keys are sorted lexicographically
@@ -121,7 +121,7 @@ def write_partial_index(index, docid, fh):
             byteorder="little",
             signed=False
         ))
-        fh.seek(0, end_offset)
+        fh.seek(end_offset)
         fh.truncate(end_offset)
         raise e
 
