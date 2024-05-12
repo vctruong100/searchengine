0ca9e3486c050dfb9821396e7be2183536e9a42c
VcTruong <vctruong100@gmail.com>
2024-05-11 21:37:26 -0700

    print_result: Fix docID print
    Remove _get_header, use seek and read to get docID.

--- print_result.py
+++ print_result.py
@@ -6,7 +6,6 @@
 
 import sys
 import os
-from index.writer import _get_header
 
 USAGE_MSG = "usage: python print_result.py indexfile"
 
@@ -16,31 +15,33 @@ def print_result(indexfile):
     :param indexfile str: The index file
 
     """
-    try:
-        with open(indexfile, 'r') as fh:
-            partcnt, docid = _get_header(fh)
-            # Read the keycnt field from the merged index file to get the number of unique words
-            fh.seek(16)
 
-            # convert the read bytes to an unsigned integer
-            # byteorder="little" - data is stored in little-endian format, least significant byte (the "little end") comes first
-            keycnt = int.from_bytes(fh.read(8), byteorder="little", signed=False)
-            print(f"Number of unique words: {keycnt}")
-            print(f"Number of documents: {docid}")
-    except Exception as e:
-        print("Error:", e)
-        sys.exit(1)
+    with open(indexfile, 'rb') as fh:
+
+        fh.seek(8) # Read the docID field from the merged index file
+        docid = int.from_bytes(fh.read(8), byteorder="little", signed=False)
+
+        # Read the keycnt field from the merged index file to get the number of unique words
+        fh.seek(16)
+
+        # convert the read bytes to an unsigned integer
+        # byteorder="little" - data is stored in little-endian format, least significant byte (the "little end") comes first
+        keycnt = int.from_bytes(fh.read(8), byteorder="little", signed=False)
+        print(f"Number of unique words: {keycnt}")
+        print(f"Number of documents: {docid}")
 
     file_size = os.path.getsize(indexfile) / 1024
     print(f"Size of index file: {file_size:.2f} KB")
 
 if __name__ == "__main__":
-    try:
-        indexfile = sys.argv[1]
-        assert os.path.isfile(indexfile), USAGE_MSG
-    except Exception as e:
+    if len(sys.argv) != 2:
         print(USAGE_MSG)
-        print("Error:", e)
+        sys.exit(1)
+
+    indexfile = sys.argv[1]
+
+    if not os.path.isfile(indexfile):
+        print(f"Error: File '{indexfile}' does not exist.")
         sys.exit(1)
 
     print_result(indexfile)
