2597e271066ccbef84180659cdf515629e228ebd
VcTruong <vctruong100@gmail.com>
2024-05-13 19:34:56 -0700

    search: update run_server, add functions

--- search.py
+++ search.py
@@ -5,27 +5,48 @@
 
 # import query.abc
 import sys
+from nltk.stem import PorterStemmer
+from collections import defaultdict
+
 
 USAGE_MSG = "usage: python search.py indexfile"
 
+def load_index(indexfile):
+    """Loads the index file and returns the index object.
+    """
+    pass
 
-def run_server(path):
+def search_query(query, index):
+    """Searches the query in the index and returns the results.
+    """
+    pass
+
+def run_server(indexfile):
     """Runs the server as a long running process
     that constantly accepts user input (from the local machine).
     """
-    print("path:", path)
+    print("file:", indexfile)
+    # load the index file
+    index = load_index(indexfile)
+
     while True:
         query = input()
         # do query work
         print("received:", query)
+        if not query:
+            continue
+
+        results = search_query(query, index)
+        for result in results:
+            print(result)
 
 
 if __name__ == "__main__":
     try:
-        path = sys.argv[1]
+        indexfile = sys.argv[1]
     except Exception as e:
         print(USAGE_MSG)
         sys.exit(1)
 
-    run_server(path)
+    run_server(indexfile)
 
