2d33e7522c72e6f895dbed74f290b8acfc5d4576
vctruong100 <vctruong100@gmail.com>
2024-05-15 21:20:21 -0700

    print_result: update to pull index file in dir

--- print_result.py
+++ print_result.py
@@ -16,32 +16,24 @@ def print_result(indexfile):
 
     """
 
-    with open(indexfile, 'rb') as fh:
+    try:
+        with open(indexfile, 'rb') as fh:
+            fh.seek(8)  # Read the docID field from the merged index file
+            docid = int.from_bytes(fh.read(8), byteorder="little", signed=False)
 
-        fh.seek(8) # Read the docID field from the merged index file
-        docid = int.from_bytes(fh.read(8), byteorder="little", signed=False)
+            # Read the keycnt field from the merged index file to get the number of unique words
+            fh.seek(16)
+            keycnt = int.from_bytes(fh.read(8), byteorder="little", signed=False)
+            print(f"Number of unique words: {keycnt}")
+            print(f"Number of documents: {docid}")
 
-        # Read the keycnt field from the merged index file to get the number of unique words
-        fh.seek(16)
-
-        # convert the read bytes to an unsigned integer
-        # byteorder="little" - data is stored in little-endian format, least significant byte (the "little end") comes first
-        keycnt = int.from_bytes(fh.read(8), byteorder="little", signed=False)
-        print(f"Number of unique words: {keycnt}")
-        print(f"Number of documents: {docid}")
-
-    file_size = os.path.getsize(indexfile) / 1024
-    print(f"Size of index file: {file_size:.2f} KB")
+            file_size = os.path.getsize(indexfile) / 1024
+            print(f"Size of index file: {file_size:.2f} KB")
+    except FileNotFoundError:
+        print(f"Error: File '{indexfile}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(USAGE_MSG)
-        sys.exit(1)
-
-    indexfile = sys.argv[1]
-
-    if not os.path.isfile(indexfile):
-        print(f"Error: File '{indexfile}' does not exist.")
-        sys.exit(1)
-
+    indexfile = "index/merged_index"
     print_result(indexfile)
