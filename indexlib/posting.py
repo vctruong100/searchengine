12715328243111d17fd958b8aeb40380ac5917de
vctruong100 <vctruong100@gmail.com>
2024-05-16 15:29:06 -0700

    indexlib/posting: Add total tokens attribute

--- indexlib/posting.py
+++ indexlib/posting.py
@@ -8,9 +8,10 @@ from functools import total_ordering
 class Posting:
     size = 12
 
-    def __init__(self, docid=None, tfidf=None, fields=dict()):
+    def __init__(self, docid=None, tf=None, total_tokens=None, fields=dict()):
         self.docid = docid
-        self.tfidf = tfidf
+        self.tf = tf
+        self.total_tokens = total_tokens
         self.fields = fields
 
     def __lt__(self, other):
@@ -23,17 +24,19 @@ class Posting:
         """Updates the Posting object based on the bytes.
         """
         self.docid = int.from_bytes(seq[:8], "little")
-        self.tfidf = int.from_bytes(seq[8:12], "little")
+        self.tf = int.from_bytes(seq[8:12], "little")
+        self.total_tokens = int.from_bytes(seq[12:16], "little")
 
     def encode(self):
         """Returns the Posting in bytes.
         """
         docid = self.docid
-        tfidf = self.tfidf
+        tf = self.tf
 
         seq = bytearray()
         seq.extend(docid.to_bytes(8, "little"))
-        seq.extend(tfidf.to_bytes(4, "little"))
+        seq.extend(tf.to_bytes(4, "little"))
+        seq.extend(self.total_tokens.to_bytes(4, "little"))
 
         return bytes(seq)
 
