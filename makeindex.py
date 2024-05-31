a3fbcb10989506268ad5d5e61343fb863b3a7044
vctruong100 <vctruong100@gmail.com>
2024-05-30 22:34:27 -0700

    makeindex: add title tag, move print docid
    Indexing was very slow on some docid, just wanted to move it to check

--- makeindex.py
+++ makeindex.py
@@ -65,6 +65,8 @@ def make_partial(pagedir, partfh, partdoc):
 
                 if not content:
                     continue # empty content
+                
+                print(f"partial flush @ doc ID: {docid}", flush=True)
 
                 soup = BeautifulSoup(content, 'lxml')
                 important_tokens = set()
@@ -80,7 +82,7 @@ def make_partial(pagedir, partfh, partdoc):
                 # and mark (or highlighted) text
                 important_tags = soup.find_all([
                     'h1', 'h2', 'h3', 'h4',
-                    'b', 'strong', 'mark',
+                    'b', 'strong', 'mark', 'title',
                 ])
                 for itag in important_tags:
                     important_text = itag.get_text()
@@ -118,7 +120,6 @@ def make_partial(pagedir, partfh, partdoc):
                 # Periodically write the partial index to disk
                 if docid % doclimit == 0: # write for every 100 documents
                     write_partial(inverted_index, docs, partfh, docfh)
-                    print(f"partial flush @ doc ID: {docid}", flush=True)
 
     # Final write for any remaining documents
     if inverted_index:
@@ -215,7 +216,7 @@ if __name__ == "__main__":
     # optional arg: keep partial
     if sys.argv[dirarg] == "--keep-partial" or sys.argv[dirarg] == "-p":
         # keep partial file
-        keep_partal = True
+        keep_partial = True
         dirarg += 1
 
     try:
